647647975177ad74e5fac9d00256cde1ad356ee2
EgorKo25 <egorko25@gmail.com>
2023-10-20 08:35:11 +0300

    base struct scheduler

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,17 +1,67 @@
 package scheduler
 
-import "sync"
+import (
+	"sync"
+	"time"
+
+	"github.com/EgorKo25/APC/internal/apc"
+)
+
+var (
+	statusWait     = "Wait"
+	statusRun      = "Run"
+	statusFinished = "Finished"
+)
 
 // NewScheduler is a constructor for a Scheduler type
 func NewScheduler(maxTask uint) *Scheduler {
 	return &Scheduler{
-		qcount: maxTask,
+		qMaxCount: maxTask,
 	}
 }
 
 // Scheduler is a custom scheduler for managing working pool of tasks
 type Scheduler struct {
-	qcount uint     // total data in queue
-	RunQ   []func() // Queue of tasks
-	lock   sync.Mutex
+	qMaxCount uint    // total data in queue
+	RunQ      []*task // Queue of tasks
+	qCount    uint
+	lock      sync.Mutex
+}
+
+// InsertTask ...
+func (s *Scheduler) InsertTask(ap *apc.AP, i float64) {
+
+	t := newTask(ap, i)
+
+	s.lock.Lock()
+	s.RunQ = append(s.RunQ, t)
+	s.lock.Unlock()
+
+	t.Status = statusWait
+}
+
+func (s *Scheduler) Run() {
+	for {
+		for i, v := range s.RunQ {
+			if v.Status == statusFinished && time.Since(v.Finish) >= v.TTL {
+				s.RunQ = s.RunQ[1:]
+			}
+			if s.qCount < s.qMaxCount && v.Status == statusWait {
+
+				s.RunQ[i].Status = statusRun
+
+				s.lock.Lock()
+				s.qCount++
+				s.lock.Unlock()
+
+				go func() {
+					if s.RunQ[i].Do() {
+						s.lock.Lock()
+						s.qCount--
+						s.lock.Unlock()
+					}
+				}()
+			}
+		}
+	}
 }
