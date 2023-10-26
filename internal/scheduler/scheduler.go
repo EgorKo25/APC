5919cedfab76dc279c07e00b4025b0ad8d0b326c
EgorKo25 <egorko25@gmail.com>
2023-10-26 22:12:07 +0300

    1. fix ctx -> chan 2. change types

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,10 +1,10 @@
 package scheduler
 
 import (
+	"sort"
 	"sync"
+	"sync/atomic"
 	"time"
-
-	"github.com/EgorKo25/APC/internal/apc"
 )
 
 var (
@@ -14,7 +14,8 @@ var (
 )
 
 // NewScheduler is a constructor for a Scheduler type
-func NewScheduler(maxTask uint) *Scheduler {
+func NewScheduler(maxTask int32) *Scheduler {
+
 	return &Scheduler{
 		qMaxCount: maxTask,
 	}
@@ -22,46 +23,77 @@ func NewScheduler(maxTask uint) *Scheduler {
 
 // Scheduler is a custom scheduler for managing working pool of tasks
 type Scheduler struct {
-	qMaxCount uint    // total data in queue
-	RunQ      []*task // Queue of tasks
-	qCount    uint
-	lock      sync.Mutex
+	qMaxCount int32   // total data in queue
+	runQ      []*Task // Queue of tasks
+	qCount    int32
+
+	lock sync.Mutex
 }
 
 // InsertTask ...
-func (s *Scheduler) InsertTask(ap *apc.AP, i float64) {
+func (s *Scheduler) InsertTask(t *Task) {
 
-	t := newTask(ap, i)
+	t.Create = time.Now()
+	t.Status = statusWait
 
 	s.lock.Lock()
-	s.RunQ = append(s.RunQ, t)
+	s.runQ = append(s.runQ, t)
 	s.lock.Unlock()
 
-	t.Status = statusWait
+}
+
+func (s *Scheduler) GetSortQueue() []*Task {
+
+	tmp := s.runQ
+
+	sort.Slice(tmp, func(i, j int) bool {
+		if s.runQ[i].Status == statusRun {
+			return s.runQ[i].Status > s.runQ[j].Status
+		}
+		if s.runQ[j].Status == statusRun {
+			return s.runQ[i].Status < s.runQ[j].Status
+		}
+		if s.runQ[i].Status == statusFinished {
+			return s.runQ[i].Status < s.runQ[j].Status
+		}
+		if s.runQ[j].Status == statusFinished {
+			return s.runQ[i].Status > s.runQ[j].Status
+		}
+		return s.runQ[i].Status == s.runQ[j].Status
+	})
+	return tmp
 }
 
 func (s *Scheduler) Run() {
+	c := make(chan struct{})
+
 	for {
-		for i, v := range s.RunQ {
-			if v.Status == statusFinished && time.Since(v.Finish) >= v.TTL {
-				s.RunQ = s.RunQ[1:]
+		s.lock.Lock()
+		for i, v := range s.runQ {
+			if v.Status == statusFinished && time.Since(v.Finish) >= v.TTL*time.Second {
+
+				s.runQ = s.runQ[1:]
+
 			}
 			if s.qCount < s.qMaxCount && v.Status == statusWait {
 
-				s.RunQ[i].Status = statusRun
+				s.runQ[i].Status = statusRun
 
-				s.lock.Lock()
-				s.qCount++
-				s.lock.Unlock()
+				atomic.AddInt32(&s.qCount, 1)
 
-				go func() {
-					if s.RunQ[i].Do() {
-						s.lock.Lock()
-						s.qCount--
-						s.lock.Unlock()
-					}
-				}()
+				go s.runQ[i].Do(c)
+
 			}
+
 		}
+		s.lock.Unlock()
+
+		select {
+		case <-c:
+			atomic.AddInt32(&s.qCount, -1)
+			continue
+		default:
+		}
+
 	}
 }
