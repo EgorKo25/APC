11502cdfd8bb62b7689b92bc425df22fed96800e
EgorKo25 <egorko25@gmail.com>
2023-10-26 22:09:31 +0300

    handlers have been written

--- internal/server/handler/handler.go
+++ internal/server/handler/handler.go
@@ -1,27 +1,77 @@
 package handler
 
 import (
+	"encoding/json"
+	"io"
+	"log"
 	"net/http"
+
+	"github.com/EgorKo25/APC/internal/scheduler"
 )
 
-func NewHandler() *Handler {
-	return &Handler{}
+// NewHandler constructor Handler
+func NewHandler(s *scheduler.Scheduler) *Handler {
+	return &Handler{s: s}
 }
 
 type Handler struct {
+	s *scheduler.Scheduler
 }
 
-func (h *Handler) MainPage(w http.ResponseWriter, r *http.Request) {
+// MainPage return / page
+func (h *Handler) MainPage(w http.ResponseWriter, _ *http.Request) {
 
-	hellomsg := `<h1>Добро пожаловать в калькулятор вычисляющий арифметическую программию</h1>`
+	helloMsg := `<h1>Welcome to Arithmetic Progressive Calculator</h1>`
 
 	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte(hellomsg))
+	_, _ = w.Write([]byte(helloMsg))
 }
+
+// SetTaskToQueue set list of tasks to queue Scheduler
 func (h *Handler) SetTaskToQueue(w http.ResponseWriter, r *http.Request) {
 
+	var tasks []scheduler.Task
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer func() { _ = r.Body.Close() }()
+
+	err = json.Unmarshal(body, &tasks)
+	if err != nil {
+		log.Println(body)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	for _, task := range tasks {
+		h.s.InsertTask(&task)
+	}
+
+	log.Println(h.s.GetSortQueue()[0])
+	w.WriteHeader(http.StatusOK)
 }
 
-func (h *Handler) GetTasksList(w http.ResponseWriter, r *http.Request) {
+// GetTasksList return all of tasks
+func (h *Handler) GetTasksList(w http.ResponseWriter, _ *http.Request) {
 
+	w.Header().Add("Content-Type", "application/json")
+
+	tasks := h.s.GetSortQueue()
+
+	b, err := json.Marshal(tasks)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	return
 }
